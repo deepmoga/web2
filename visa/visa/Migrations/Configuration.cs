3e4d189e3b43fc90fccfef2d47fcae70537ae71c
deepmoga <32519872+deepmoga@users.noreply.github.com>
2017-10-23 17:04:27 +0530

    changes

--- visa/visa/Migrations/Configuration.cs
+++ visa/visa/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace visa.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(visa.Models.dbcontext context)
